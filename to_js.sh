8601827d0f3fd565bfd2429f8c2344cd9df613b0
bootstraponline <cafe@bootstraponline.com>
2012-07-06 15:22:56 -0600

    Debug.

--- to_js.sh
+++ to_js.sh
@@ -11,5 +11,9 @@ mv sundown sundown.bc
 echo "Converting sundown.bc "
 # emcc sundown.bc -o sundown_o0.js
 # emcc -O1 sundown.bc -o sundown_o1.js
-emcc -O2 sundown.bc -o sundown_o2.js
+# emcc -O2 sundown.bc -o sundown_o2.js
 # emcc -O3 sundown.bc -o sundown_o3.js
+emcc -O2 --minify 0 sundown.bc -o sundown_o2_m0.js
+emcc -O2 --closure 0 sundown.bc -o sundown_o2_c0.js
+emcc -O2 --minify 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_m0_a1.js
+emcc -O2 --closure 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_c0_a1.js
