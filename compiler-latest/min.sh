7d01245868f8e923d65f6137c27718ac2baf829a
bootstraponline <cafe@bootstraponline.com>
2012-07-06 23:33:23 -0600

    Remove whitespace using compiler.jar.

---
+++ compiler-latest/min.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar compiler.jar --compilation_level WHITESPACE_ONLY --js sundown_o2_m0.js --js_output_file sundown_o2_wo.js
--- to_js.sh
+++ to_js.sh
@@ -18,3 +18,6 @@ emcc -O2 --minify 0 sundown.bc -o sundown_o2_m0.js
 # emcc -O2 --minify 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_m0_a1.js
 # emcc -O2 --closure 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_c0_a1.js
 # emcc -O2 -s ASSERTIONS=1 sundown.bc -o sundown_o2_a1.js
+
+echo "Removing whitespace (minify 1 is broken)"
+java -jar ./compiler-latest/compiler.jar --compilation_level WHITESPACE_ONLY --js sundown_o2_m0.js --js_output_file sundown_o2_wo.js
