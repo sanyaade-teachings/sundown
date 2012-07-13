dd3bd68c1c243234129a038c05acb31f96c7ea36
bootstraponline <cafe@bootstraponline.com>
2012-07-13 13:46:27 -0600

    c0

--- to_js.sh
+++ to_js.sh
@@ -13,14 +13,10 @@ echo "Converting sundown.bc "
 # emcc -O1 sundown.bc -o sundown_o1.js
 # emcc -O2 sundown.bc -o sundown_o2.js
 # emcc -O3 sundown.bc -o sundown_o3.js
-emcc -O2 --minify 0 sundown.bc -o sundown_o2_m0.js
+# emcc -O2 --minify 0 sundown.bc -o sundown_o2_m0.js
 # emcc -O2 --closure 0 sundown.bc -o sundown_o2_c0.js
 # emcc -O2 --minify 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_m0_a1.js
 # emcc -O2 --closure 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_c0_a1.js
 # emcc -O2 -s ASSERTIONS=1 sundown.bc -o sundown_o2_a1.js
 
-echo "Removing whitespace (minify 1 is broken)"
-java -jar ./compiler-latest/compiler.jar --compilation_level WHITESPACE_ONLY --js sundown_o2_m0.js --js_output_file sundown_o2_wo.js
-
-mv sundown_o2_m0.js ./dist
-mv sundown_o2_wo.js ./dist
+emcc -O2 --closure 0 sundown.bc -o sundown_o2_c0.js
