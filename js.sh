b0afe6f06c55b9d7006349f6eaa3957ae6595ebd
bootstraponline <cafe@bootstraponline.com>
2012-07-14 21:32:26 -0600

    minify 0

--- js.sh
+++ js.sh
@@ -20,4 +20,4 @@ echo "Converting sundown.bc "
 # emcc -O2 -s ASSERTIONS=1 sundown.bc -o sundown_o2_a1.js
 
 # emcc -O2 sundown.bc -o sundown.js -s EXPORTED_FUNCTIONS="['_main', '_malloc', '_free', '_str_to_html', '_realloc']"
-emcc -O2 --closure 0 sundown.bc -o sundown.js -s EXPORTED_FUNCTIONS="['_main', '_malloc', '_free', '_str_to_html', '_realloc']"
+emcc -O2 --minify 0 --closure 0 sundown.bc -o sundown.js -s EXPORTED_FUNCTIONS="['_main', '_malloc', '_free', '_str_to_html', '_realloc']"
