72b7c879635bd3b0620553aa5a14e3c66de13bec
bootstraponline <cafe@bootstraponline.com>
2012-07-14 20:51:28 -0600

    Add test files.

---
+++ testjs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Cleaning"
+make clean
+
+echo "Generating bit code"
+make -f Makefile.js.test
+
+echo "Adding .bc extension to sundown"
+mv libsundown.so sundown.bc
+
+echo "Converting sundown.bc "
+# emcc sundown.bc -o sundown_o0.js
+# emcc -O1 sundown.bc -o sundown_o1.js
+# emcc -O2 sundown.bc -o sundown_o2.js
+# emcc -O3 sundown.bc -o sundown_o3.js
+# emcc -O2 --minify 0 sundown.bc -o sundown_o2_m0.js
+# emcc -O2 --closure 0 sundown.bc -o sundown_o2_c0.js
+# emcc -O2 --minify 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_m0_a1.js
+# emcc -O2 --closure 0 -s ASSERTIONS=1 sundown.bc -o sundown_o2_c0_a1.js
+# emcc -O2 -s ASSERTIONS=1 sundown.bc -o sundown_o2_a1.js
+
+emcc -O2 sundown.bc -o test.js --embed-file data.md
+
+echo "Saving JS output"
+node test.js data.md > output.js.html
+
